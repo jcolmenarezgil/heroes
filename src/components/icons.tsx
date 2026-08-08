e9e61489d82902410ab6f82a04745689827ae760
jcolmenaresgil <jose.colmenares.gil@gmail.com>
2026-08-08 17:31:04 -0400

    `feat(about): add open source license and community contributors section`

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -294,3 +294,43 @@ export const ArrowPathIcon = ({ className }: IconProps) => (
     />
   </Icon>
 );
+
+export const GitForkIcon = ({ className }: IconProps) => (
+  <Icon className={className}>
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 12v3.75m0 0a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5zm0 0V12m0 0a3.75 3.75 0 013.75-3.75h1.5m-5.25 3.75A3.75 3.75 0 006.75 8.25h-1.5M18 8.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5zM6 8.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5z"
+    />
+  </Icon>
+);
+
+export const HeartIcon = ({ className }: IconProps) => (
+  <Icon className={className}>
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+    />
+  </Icon>
+);
+
+export const DocumentTextIcon = ({ className }: IconProps) => (
+  <Icon className={className}>
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+    />
+  </Icon>
+);
+
+export const CodeBracketIcon = ({ className }: IconProps) => (
+  <Icon className={className}>
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
+    />
+  </Icon>
+);
